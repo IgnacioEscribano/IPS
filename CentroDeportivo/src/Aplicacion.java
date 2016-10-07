68d970bce315f83fcafac9d0804319e0ca645866
uo180365 <uo180365@EII-L-11-13.aulas.uniovi.es>
2016-10-07 15:48:08 +0200

    Prueba modificacion. No me deja crear nuevos .java por algun motivo.

--- CentroDeportivo/src/Aplicacion.java
+++ CentroDeportivo/src/Aplicacion.java
@@ -7,5 +7,6 @@ public class Aplicacion {
     public static void main(String[] args) {
         CreadorConexionBBDD creadorConexionBBDD = new CreadorConexionBBDD();
         Connection conn = creadorConexionBBDD.getConexion();
+        //prueba modif
     }
 }
